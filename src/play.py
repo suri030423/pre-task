ec9be8adb678791985a089825375744579ebc388
김지현 <suri0323@naver.com>
2025-12-05 01:19:01 +0900

    update

--- src/play.py
+++
@@ -1,5 +0,0 @@
-from dataloader import SegmentationDataset
-ds = SegmentationDataset("data/train/images", "data/train/masks")
-len(ds)
-img, mask = ds[0]
-img.shape, img.dtype, mask.shape, mask.dtype
